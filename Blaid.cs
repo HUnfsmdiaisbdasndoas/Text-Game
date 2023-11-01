906b9d7a9f9adb1b1a1e80072cb69247d0bf2623
Jamal Tendentious Jr. III <144495434+HUnfsmdiaisbdasndoas@users.noreply.github.com>
2023-11-01 15:22:49 -0700

    Update Blaid.cs

--- Blaid.cs
+++ Blaid.cs
@@ -5,5 +5,16 @@ class Program
   static void Main()
 	{
 		Console.WriteLine("Give me rent");
+	  	int Answer = int.Parse(Console.ReadLine()); 
+
+	  	if (Asnwer == yes) 
+		{
+			Console.WriteLine("OK");
+		}
+	  	else (Answer == no) 
+		{
+			Console.WriteLine("Broke Ass Nigga");
+		}
+			
 	}
 }
